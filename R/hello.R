579bc15f7bf990cf647beb93d8e1c0eb8727ec08
Cankun Wang <flykun0620@gmail.com>
2019-12-04 16:36:35 -0500

    Add: init object class

--- R/hello.R
+++ R/hello.R
@@ -12,52 +12,53 @@
 #' If "Build Package Documentation" shortcut not working, check within Tools -> Project Options... -> Build Tools; there should be a checkbox:
 #' @examples
 #' hello()
-#'
-
 hello <- function(x) {
-print("Install Package:           'Ctrl + Shift + B'")
-print("Check Package:             'Ctrl + Shift + E'")
-print("Test Package:              'Ctrl + Shift + T'")
-print("Make Document:             'Ctrl + Shift + D'")
-print("Knit document:              knitr::knit('README.Rmd')")
-#knitr::knit('README.Rmd')
+  message("Install Package:           'Ctrl + Shift + B'")
+  message("Check Package:             'Ctrl + Shift + E'")
+  message("Test Package:              'Ctrl + Shift + T'")
+  message("Make Document:             'Ctrl + Shift + D'")
+  message("Knit document:              knitr::knit('README.Rmd')")
+  message("For loop, initialize the progress bar with:              pb <- txtProgressBar(min = 1, max = 100, char = "=", style = 3)")
+  message("update in each loop iteration with:              setTxtProgressBar(pb = pb, value = i)")
+
+  # knitr::knit('README.Rmd')
 }
 
 #' @title test1
 #' @description test script
-test1 <- function(){
-  #s3 <- read.fasta("bic2337.txt.fa")
+test1 <- function() {
+  # s3 <- read.fasta("bic2337.txt.fa")
   s1 <- DNAString("ATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTG")
   s1
   s4 <- DNAString("ATCGA")
   s1
-  s2 <- DNAString(paste0(rep(s1, 20000), collate=""))
+  s2 <- DNAString(paste0(rep(s1, 20000), collate = ""))
   s2
 
   matchPattern(s4, enhancerFB)
 
   data(TFBS)
   DNAString(s3$ENSG00000108179)
-  sk13 <- spectrumKernel(k=14, normalized=FALSE)
+  sk13 <- spectrumKernel(k = 14, normalized = FALSE)
   system.time(kmerFreq <- drop(getExRep(s1, sk13)))
   kmerFreq
-  system.time(kmerFreq <- drop(getExRep(rep(enhancerFB,20), sk13)))
-  kmerFreq <- getExRep(rep(enhancerFB,10), sk13)
-  kmerFreq[1:10,1:10]
+  system.time(kmerFreq <- drop(getExRep(rep(enhancerFB, 20), sk13)))
+  kmerFreq <- getExRep(rep(enhancerFB, 10), sk13)
+  kmerFreq[1:10, 1:10]
   append()
 }
 
 #' @title test2
 #' @description test script
-test2 <- function(){
-  #s3 <- read.fasta("bic2337.txt.fa")
+test2 <- function() {
+  # s3 <- read.fasta("bic2337.txt.fa")
   s1 <- Biostrings::DNAString("ATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTGATCGATCGATCGATCGATCGATCGACTGACTAGCTAGCTACGATCGACTG")
 
   s4 <- Biostrings::DNAString("ATCGA")
 
-  s2 <- Biostrings::DNAString(paste0(rep(s1, 20), collate=""))
+  s2 <- Biostrings::DNAString(paste0(rep(s1, 20), collate = ""))
 
-  sk13 <- kebabs::spectrumKernel(k=14, normalized=FALSE)
+  sk13 <- kebabs::spectrumKernel(k = 14, normalized = FALSE)
   system.time(kmerFreq <- drop(kebabs::getExRep(s1, sk13)))
   kmerFreq
 }
